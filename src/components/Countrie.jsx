6811195167241921fc6c76db118553e753596afe
Moksleivis <Moksleivis@kitm.local>
2023-06-01 15:04:33 +0300

    linko pataisymas

--- src/components/Countrie.jsx
+++ src/components/Countrie.jsx
@@ -13,7 +13,7 @@ const Countrie = ({ countries }) => {
                     <Card style={{ width: '18rem' }}>
                         <Card.Img variant="top" src="holder.js/100px180" />
                         <Card.Body>
-                            <Card.Title>gg</Card.Title>
+                            <Card.Title>{country.name.common}</Card.Title>
                             <Card.Text>
                                 Some quick example text to build on the card title and make up the
                                 bulk of the card's content.
--- src/services/CountriesService.js
+++ src/services/CountriesService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = 'https://restcountries.com/v3.1/allll';
+const API_URL = 'https://restcountries.com/v3.1/all';
 
 const getAllCountriesInfo = async() => {
   try {
