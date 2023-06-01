8c0922f3c62869d9264c2256597c0ea2a421cd44
Kestutis Masaitis <kmngeru@gmail.com>
2023-06-01 20:45:57 +0300

    stiliuko patobulinimas

--- src/components/Countrie.jsx
+++ src/components/Countrie.jsx
@@ -7,12 +7,12 @@ const Countrie = ({ countries }) => {
 
 
     return (
-        <div className='d-inline-flex flex-wrap justify-content-center'>
+        <div className='d-inline-flex flex-wrap justify-content-center bg-dark'>
             {countries !== undefined && countries.length !== 0 ?
                 countries.map((country, index) => (
-                    <Card className='m-3 ' style={{ width: '18rem' }} key={index}>
-                        <Card.Img variant="top" src={country.flags.png} alt={country.flags.alt} />
-                        <Card.Body>
+                    <Card className='m-3 border-secondary text-bg-secondary' style={{ width: '18rem' }} key={index}>
+                        <Card.Img className='border-bottom border-secondary' variant="top" src={country.flags.png} alt={country.flags.alt} />
+                        <Card.Body className='d-inline-flex flex-column justify-content-around'>
                             <Card.Title>{country.name.common}</Card.Title>
                             <Card.Text>Region: {country.region} ({country.subregion})</Card.Text>
                             <Button variant="primary">Read more about - {country.name.common}</Button>
